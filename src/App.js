5c6b3220a009b86958bb63b75f3ddb39a32a8b49
bodisB <bencebodis07@gmail.com>
2021-06-28 15:37:44 +0200

    add footer with a link to the rules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CardContainer from "./components/cards/CardContainer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <CardContainer />
+      <Footer />
     </div>
   );
 }
