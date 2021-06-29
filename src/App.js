048528997dcec05098fe8b2eef7145ab5637a81e
bodisB <bencebodis07@gmail.com>
2021-06-29 16:23:43 +0200

    feat: add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Login from "./components/Login";
+import Registration from "./components/Registration";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import CardContainer from "./components/cards/CardContainer";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Redirect to="/login" />;
+          }}
+        />
+        <Route path="/login" component={Login} />
+        <Route path="/registration" component={Registration} />
+        <Route path="/cards" component={CardContainer} />
+      </Router>
     </div>
   );
 }
