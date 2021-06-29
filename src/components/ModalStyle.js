25dc596bafce8689d983ec471b47b65bc0ce440c
bodisB <bencebodis07@gmail.com>
2021-06-29 16:22:25 +0200

    feat: add style to login, registration components

--- src/components/ModalStyle.js
+++ src/components/ModalStyle.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Modal = styled.div`
   display: block; /* Hidden by default */
@@ -7,17 +8,62 @@ export const Modal = styled.div`
   top: 0;
   width: 100%; /* Full width */
   height: 100%; /* Full height */
-  background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+`;
+
+export const SubmitButton = styled.button`
+  margin-bottom: 20px;
+  margin-left: 10%;
+  margin-right: 10%;
+  color: white;
+  background-color: #a122a1;
+  width: 80%;
+  height: 40px;
+`;
+
+export const Input = styled.input`
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 78%;
+  padding-bottom: 20px;
+`;
+
+export const RegDiv = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+export const RegLink = styled(Link)`
+  margin-right: 10%;
+  margin-left: auto;
+  margin-bottom: 30px;
+  color: black;
+`;
+
+export const H2 = styled.h2`
+  text-align: center;
+  color: black;
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 25%;
+  padding-top: 20px;
+`;
+
+export const H1 = styled.h1`
+  text-align: center;
+  color: #a122a1;
+  margin-left: 10%;
+  margin-right: 10%;
+`;
+
+export const Label = styled.label`
+  margin-left: 10%;
+  color: grey;
 `;
 
 export const ModalContent = styled.div`
-  background-color: #2c5d72;
-  margin: 15% auto; /* 15% from the top and centered */
-  padding: 20px;
-  border: 1px solid #d3b509;
+  background-color: white;
+  margin: 5% auto; /* 15% from the top and centered */
   width: 25%;
-  text-align: center;
-  border-radius: 30px;
-  color: #d3b509;
 `;
