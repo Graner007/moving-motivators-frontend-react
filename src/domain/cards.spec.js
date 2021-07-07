2e8257f7afa6d472e1f764bf9b92eff7fe802029
Bence Fábián <begnoc@gmail.com>
2021-07-07 14:34:36 +0200

    add domain files

---
+++ src/domain/cards.spec.js
@@ -0,0 +1,53 @@
+const example_horizontal = {
+  'stage':'horizontal',
+  'cards': [
+    'acceptance', 'curiosity', 'freedom', 'goal', 'honor',
+    'mastery', 'order', 'power', 'relatedness', 'status'
+  ]
+}
+
+const example_vertical = {
+  'stage': 'vertical',
+  'cards': [
+    {
+      'name': 'order',
+      'position': 'middle'
+    },
+    {
+      'name': 'acceptance',
+      'position': 'high'
+    },
+    {
+      'name': 'curiosity',
+      'position': 'middle'
+    },
+    {
+      'name': 'freedom',
+      'position': 'middle'
+    },
+    {
+      'name': 'goal',
+      'position': 'middle'
+    },
+    {
+      'name': 'honor',
+      'position': 'middle'
+    },
+    {
+      'name': 'mastery',
+      'position': 'middle'
+    },
+    {
+      'name': 'power',
+      'position': 'high'
+    },
+    {
+      'name': 'relatedness',
+      'position': 'middle'
+    },
+    {
+      'name': 'status',
+      'position': 'low'
+    }
+  ]
+}
